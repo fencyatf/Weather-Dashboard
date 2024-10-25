a9e34550962f5c854bdd9d13c253f3da8b746ee5
fency <fencyatf@gmail.com>
2024-10-25 13:28:20 +0530

    Modification in temperature

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ searchBtn.addEventListener('click',() =>{
    
     document.getElementById('error').style.display = 'none';
     //show the spinner
-    const loading = document.getElementById('loading');
-    loading.style.display = 'block';
-
-    //fetch details from API
-    fetchWeather(city)
-    .then(data =>{
+        const loading = document.getElementById('loading');
+        loading.style.display = 'block';
+    setTimeout(() =>{
+        fetchWeather(city)
+            .then(data =>{
         // console.log(data)
         //Populate the weather results
         //function to update card background
-        updateCardBackground(data.weather[0].main);
+            const temperature = data.main.temp;
+
+            document.getElementById('cityName').innerText = data.name;
+            document.getElementById('temperature').innerText = temperature;
+            document.getElementById('weather').innerText = data.weather[0].description;
+            document.getElementById('humidity').innerText = data.main.humidity;
+            document.getElementById('wind').innerText = data.wind.speed;
+    
+            document.getElementById('weather-details').style.display = 'block';
+            updateCardBackground(temperature);
+    
     })
     .catch(err =>{
         //handle display of error msg
@@ -27,12 +36,16 @@ searchBtn.addEventListener('click',() =>{
         //Stop the spinner
         loading.style.display = 'none';
     })
+    },1000)
+
+    //fetch details from API
+    
 })
 
 function fetchWeather(city){
 
     const apiKey = 'befd984de4d3c050671d4eb935e6c660'; 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     return fetch(url)
     .then(response => {
@@ -41,31 +54,21 @@ function fetchWeather(city){
         }
         return response.json()
     })
-    .then(data =>{
-        document.getElementById('cityName').innerText = data.name;
-        document.getElementById('temperature').innerText = Math.round((data.main.temp - 273.15).toFixed(2));
-        document.getElementById('weather').innerText = data.weather[0].description;
-        document.getElementById('humidity').innerText = data.main.humidity;
-        document.getElementById('wind').innerText = data.wind.speed;
-
-        document.getElementById('weather-details').style.display = 'block';
-        return data;
-    })
 }
 
-function updateCardBackground(weatherMain) {
+function updateCardBackground(temperature) {
     const weatherDetails = document.getElementById('weather-details');
 
-    console.log(weatherMain);
+    console.log(temperature);
     
     weatherDetails.classList.remove('sunny', 'cloudy', 'rainy','default-background');
 
     
-    if (weatherMain === 'Clear') {
+    if (temperature > 30) {
         weatherDetails.classList.add('sunny');
-    } else if (weatherMain === 'Clouds') {
+    } else if (temperature >= 20 && temperature <= 30) {
         weatherDetails.classList.add('cloudy');
-    } else if (weatherMain === 'Rain') {
+    } else if (temperature < 20) {
         weatherDetails.classList.add('rainy');
     }else {
         weatherDetails.classList.add('default-background');
